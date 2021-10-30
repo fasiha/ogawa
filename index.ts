75920993849b7b3ffc8d6270dc35affef326f1d7
Ahmed Fasih <wuzzyview@gmail.com>
2021-10-30 11:19:34 -0700

    Fix black marble as usual

--- index.ts
+++ index.ts
@@ -2,9 +2,39 @@ import * as Cesium from 'cesium';
 Cesium.Ion.defaultAccessToken =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmNTlhMDg1Mi1hMTU3LTQwMWEtYThmMy1hZmRmZmNhMzk1ZmIiLCJpZCI6NzE5MDcsImlhdCI6MTYzNTU2ODQwMn0.LBKVBGjIpqU9rfeIJO4T6j81tG2nXqyFvz9D3JgXHFA';
 
-var viewer =
-    new Cesium.Viewer('cesiumContainer', {animation: false, timeline: false});
+// For some reason this is omitted from Cesium TypeScript defs: TODO open an
+// issue
+const createDefaultImageryProviderViewModels =
+    (Cesium as (typeof Cesium) & {
+      createDefaultImageryProviderViewModels: () => Cesium.ProviderViewModel[]
+    }).createDefaultImageryProviderViewModels;
+
+// The Black Marble model from Cesium
+// (https://cesiumjs.org/data-and-assets/imagery/black-marble/) is sometimes
+// blocked due to CORS, so load it from GitHub Pages.
+var models = createDefaultImageryProviderViewModels().filter(
+    model => model.name !== 'Earth at night');
+var model = new Cesium.ProviderViewModel({
+  name: 'Black Marble Night Lights',
+  iconUrl: 'Widgets/Images/ImageryProviders/earthAtNight.png',
+  tooltip:
+      'Nighttime view of the Earth, collected by the Suomi NPP satellite in 2012',
+  creationFunction: function() {
+    return new Cesium.TileMapServiceImageryProvider({
+      url: 'https://fasiha.github.io/nasa-black-marble-tiles',
+      credit: new Cesium.Credit('NASA Night Lights 2012')
+    });
+  }
+});
+models.push(model);
+
+// Create the viewer!
+var viewer = new Cesium.Viewer(
+    'cesiumContainer',
+    {animation: false, timeline: false, imageryProviderViewModels: models});
 var imageryLayers = viewer.imageryLayers;
+
+// Add our texture-shaded elevation!
 function addAdditionalLayerOption(
     name: string, imageryProvider: Cesium.TileMapServiceImageryProvider,
     alpha: number, contrast: number) {
@@ -23,6 +53,7 @@ var tmsProvider = new Cesium.TileMapServiceImageryProvider({
 });
 var tms = addAdditionalLayerOption('TMS', tmsProvider, 0.75, 1.4);
 
+// Create some controls!
 var ul = document.createElement('ul');
 var params = {
   brightness: 1,
